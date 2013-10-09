614ab31af817fa9775fe2aa904687456656bf6fc
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-10-09 18:30:07 -0300

    Set except import error on add introspection rules south

--- tags/fields.py
+++ tags/fields.py
@@ -40,6 +40,6 @@ class TagField(CharField):
 try:
     from south.modelsinspector import add_introspection_rules
     add_introspection_rules([], ["^tags\.fields\.TagField"])
-except:
+except ImportError:
     pass
 
