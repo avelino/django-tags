bfde4e3f0329545bc573a8ca4deb6f092b42b2cb
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-10-09 05:31:45 -0300

    add basic tests to create `Food`

---
+++ tests/tests.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, absolute_import
+
+from django.test import TestCase
+
+from tags.models import Tag
+
+from .models import Food
+
+
+class TestFoodModel(TestCase):
+
+    def test_create_food(self):
+        food = Food.objects.create(
+            name="nacho",
+            tags="tortilla chips")
+
+        self.assertTrue(food)
+        self.assertEqual(Tag.objects.all()[0].name, "tortilla chips")
+        self.assertEqual(Tag.objects.all()[0].slug, "tortilla-chips")
+
+    def test_create_two_tags(self):
+        food = Food.objects.create(
+            name="nacho",
+            tags="tortilla chips, salsa")
+        tags = Tag.objects.all()
+
+        self.assertTrue(food)
+        self.assertEqual(len(tags), 2)
+        self.assertEqual(tags[1].name, "tortilla chips")
+        self.assertEqual(tags[1].slug, "tortilla-chips")
+        self.assertEqual(tags[0].name, " salsa")
+        self.assertEqual(tags[0].slug, "salsa")
